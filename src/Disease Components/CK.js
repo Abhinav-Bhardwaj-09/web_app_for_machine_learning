72a1f8235e3495eb1016cc3f4305b558753e3140
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-27 20:07:57 +0530

    Added a back button

--- src/Disease Components/CK.js
+++ src/Disease Components/CK.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./CK.css";
 
 const CK = () => {
-  return <div>CK Component</div>;
+  return (
+    <div>
+      <NavLink to="/Home" exact style={{ textDecoration: "none" }}>
+        <div className="arrow-left"></div>
+      </NavLink>
+      CK Component
+    </div>
+  );
 };
 
 export default CK;
