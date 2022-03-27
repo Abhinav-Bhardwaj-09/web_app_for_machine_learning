169cb7ade68f512e238085a4e8e72b31d9dc8e7f
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-27 20:11:27 +0530

    Added a back button

--- src/Disease Components/BT.js
+++ src/Disease Components/BT.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./BT.css";
 
 const BT = () => {
-  return <div>BT Component</div>;
+  return (
+    <div>
+      <NavLink to="/Home" exact style={{ textDecoration: "none" }}>
+        <div className="arrow-left"></div>
+      </NavLink>
+
+      <h1 className="dheading">Brain &nbsp;Tumor &nbsp; Predictor</h1>
+      <br />
+    </div>
+  );
 };
 
 export default BT;
