82843beb6a1c69cb8d0b4827f221dca619a8e235
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-05-08 23:43:09 +0530

    Renamed BT component to BC routes

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import HA from "./Disease Components/HA";
-import BT from "./Disease Components/BT";
+import BC from "./Disease Components/BC";
 import CK from "./Disease Components/CK";
 import AL from "./Disease Components/AL";
-
-import "./Routing.css";
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/HeartAttack" element={<HA />} />
-        <Route exact path="/BrainTumor" element={<BT />} />
+        <Route exact path="/BreastCancer" element={<BC />} />
         <Route exact path="/ChronicKidney" element={<CK />} />
         <Route exact path="/AcuteLiver" element={<AL />} />
         <Route exact path="/*" element={<Home />} />
