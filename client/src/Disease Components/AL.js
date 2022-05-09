1b4079fe6b495c72c040faf135157996cc9f83a2
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-05-09 23:22:42 +0530

    Added few attributes in form

--- client/src/Disease Components/AL.js
+++ client/src/Disease Components/AL.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./AL.css";
@@ -12,61 +12,122 @@ const AL = () => {
       <h1 className="dheading">
         Acute &nbsp;Liver &nbsp;Disease &nbsp; Predictor
       </h1>
-      <form className="formStylingAL">
+      <form className="formStylingAL" method="POST">
         <div className="row rHA">
           <div className="col cHA">
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="age">
               Age :&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="age"
+                name="age"
+              />
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Total_Bilirubin">
               Total Bilirubin (mg/dL):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Total_Bilirubin"
+                name="Total_Bilirubin"
+              />
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Direct_Bilirubin">
               Direct Bilirubin (mg/dL):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Direct_Bilirubin"
+                name="Direct_Bilirubin"
+              />
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Aspartate_Aminotransferase">
               Aspartate Aminotransferase (IU/L):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Aspartate_Aminotransferase"
+                name="Aspartate_Aminotransferase"
+              />
             </label>
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Albumin">
               Albumin (g/dL):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Albumin"
+                name="Albumin "
+              />
             </label>
           </div>
 
           <div className="col cHA">
-            <label className="HAlabel">
-              Sex :&nbsp;&nbsp;&nbsp;
-              <select className="inputStylingAL optionssAL">
-                <option selected>Choose</option>
+            <label className="HAlabel" htmlFor="Gender">
+              Gender :&nbsp;&nbsp;&nbsp;
+              <select
+                required
+                className="inputStylingAL optionssAL"
+                id="Gender"
+                name="Gender"
+              >
+                <option selected disabled>
+                  Choose
+                </option>
                 <option value="1">Male</option>
                 <option value="0">Female</option>
               </select>
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Alkaline_Phosphotase">
               Alkaline Phosphotase (IU/L):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Alkaline_Phosphotase"
+                name="Alkaline_Phosphotase"
+              />
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Alamine_Aminotransferase">
               Alamine Aminotransferase (IU/L):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Alamine_Aminotransferase"
+                name="Alamine_Aminotransferase"
+              />
             </label>
 
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Total_Protiens">
               Total Protiens (g/dL):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Total_Protiens"
+                name="Total_Protiens"
+              />
             </label>
-            <label className="HAlabel">
+            <label className="HAlabel" htmlFor="Albumin_and_Globulin_Ratio">
               Albumin and Globulin Ratio (A/G):&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStylingAL" />
+              <input
+                required
+                type="number"
+                className="inputStylingAL"
+                id="Albumin_and_Globulin_Ratio"
+                name="Albumin_and_Globulin_Ratio"
+              />
             </label>
           </div>
         </div>
