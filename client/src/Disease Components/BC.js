61cdc417f5d206357126c54705c19614631fe70b
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-05-17 21:26:35 +0530

    Breast Cancer component with form

--- client/src/Disease Components/BC.js
+++ client/src/Disease Components/BC.js
@@ -10,131 +10,374 @@ const BT = () => {
         <div className="arrow-left"></div>
       </NavLink>
 
-      <h1 className="dheading">Brain &nbsp;Tumor &nbsp; Predictor</h1>
+      <h1 className="dheading">Breast &nbsp;Cancer &nbsp; Predictor</h1>
       <br />
-      <form className="formStyling">
-        <div className="row rBT">
-          <div className="col cBT">
-            <label className="BTlabel">
-              Age :&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStyling" />
+      <form className="formStylingBC" method="POST">
+        <div className="row rBC">
+          <div className="col cBC">
+            <label className="BClabel">
+              Radius Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="radius_mean"
+                name="radius_mean"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Resting BP (mm Hg) :&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStyling" />
+            <label className="BClabel">
+              Texture Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="texture_mean"
+                name="texture_mean"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Chest Pain Type :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="0">Asymptomatic</option>
-                <option value="1">Atypical Angina</option>
-                <option value="2">Non-atypical Angina</option>
-                <option value="3">Typical Angina</option>
-              </select>
+            <label className="BClabel">
+              Perimeter Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="perimeter_mean"
+                name="perimeter_mean"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Slope of the Peak Exercise ST Segment :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="0">Downsloping</option>
-                <option value="1">Flat</option>
-                <option value="2">Upsloping</option>
-              </select>
+            <label className="BClabel">
+              Area Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="area_mean"
+                name="area_mean"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Number of Major Vessel(s) :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
+            <label className="BClabel">
+              Smoothness Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="smoothness_mean"
+                name="smoothness_mean"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Resting Electrocardiographic :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="0">
-                  Probable or Definite Left Ventricular Hypertrophy by
-                  Estes&#39; Criteria
-                </option>
-                <option value="1">Normal</option>
-                <option value="2">
-                  ST-T Wave Abnormality (T wave inversions and/or ST elevation
-                  or depression of &#62; 0.05 mV)
-                </option>
-              </select>
+            <label className="BClabel">
+              Compactness Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="compactness_mean"
+                name="compactness_mean"
+                step="any"
+              />
+            </label>
+            <label className="BClabel">
+              Concavity Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concavity_mean"
+                name="concavity_mean"
+                step="any"
+              />
+            </label>
+            <label className="BClabel">
+              Concave Points Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concave_points_mean"
+                name="concave_points_mean"
+                step="any"
+              />
+            </label>
+            <label className="BClabel">
+              Symmetry Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="symmetry_mean"
+                name="symmetry_mean"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Fractal Dimension Mean :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="fractal_dimension_mean"
+                name="fractal_dimension_mean"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Radius se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="radius_se"
+                name="radius_se"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Texture se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="texture_se"
+                name="texture_se"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Perimeter se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="perimeter_se"
+                name="perimeter_se"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Area se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="area_se"
+                name="area_se"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Smoothness se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="smoothness_se"
+                name="smoothness_se"
+                step="any"
+              />
             </label>
           </div>
 
-          <div className="col cBT">
-            <label className="HAlabel">
-              Sex :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="1">Male</option>
-                <option value="0">Female</option>
-              </select>
+          <div className="col cBC">
+            <label className="BClabel">
+              Compactness se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="compactness_se"
+                name="compactness_se"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Cholesterol (mg/dl) :&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStyling" />
+            <label className="BClabel">
+              Concavity se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concavity_se"
+                name="concavity_se"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Exercise induced Angina :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="1">Yes</option>
-                <option value="0">No</option>
-              </select>
+            <label className="BClabel">
+              Concave Points se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concave_points_se"
+                name="concave_points_se"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Is Fasting Blood Sugar Level &#62; 120 mg/dl ? :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="1">True</option>
-                <option value="0">False</option>
-              </select>
+            <label className="BClabel">
+              Symmetry se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="symmetry_se"
+                name="symmetry_se"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Blood Disorder (Thalassemia) :&nbsp;&nbsp;&nbsp;
-              <select className="inputStyling">
-                <option selected>Choose</option>
-                <option value="0">Dropped from the dataset previously</option>
-                <option value="1">
-                  Fixed Defect (No Blood Flow in some part of the Heart))
-                </option>
-                <option value="2">Normal Blood Flow</option>
-                <option value="3">
-                  Reversible Defect (Blood Flow is observed but NOT Normal)
-                </option>
-              </select>
+            <label className="BClabel">
+              Fractal Dimension se :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="fractal_dimension_se"
+                name="fractal_dimension_se"
+                step="any"
+              />
             </label>
 
-            <label className="BTlabel">
-              Maximum Heart Rate :&nbsp;&nbsp;&nbsp;
-              <input type="number" className="inputStyling" />
+            <label className="BClabel">
+              Radius Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="radius_worst"
+                name="radius_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Texture Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="texture_worst"
+                name="texture_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Perimeter Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="perimeter_worst"
+                name="perimeter_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Area Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="area_worst"
+                name="area_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Smoothness Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="smoothness_worst"
+                name="smoothness_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Compactness Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="compactness_worst"
+                name="compactness_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Concavity Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concavity_worst"
+                name="concavity_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Concave Points Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="concave_points_worst"
+                name="concave_points_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Symmetry Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="symmetry_worst"
+                name="symmetry_worst"
+                step="any"
+              />
+            </label>
+
+            <label className="BClabel">
+              Fractal Dimension Worst :&nbsp;&nbsp;&nbsp;
+              <input
+                required
+                type="number"
+                className="inputStylingBC"
+                id="fractal_dimension_worst"
+                name="fractal_dimension_worst"
+                step="any"
+              />
             </label>
           </div>
         </div>
 
-        <div className="row rBT">
-          <label className="BTlabel">
-            ST depression induced by exercise relative to rest (OldPeak)
-            :&nbsp;&nbsp;&nbsp;
-            <input type="number" className="inputStyling" />
-          </label>
+        <div className="btnContainerHA">
+          <button className="btn btn-success submitButtonHA">Submit</button>
         </div>
       </form>
     </div>
